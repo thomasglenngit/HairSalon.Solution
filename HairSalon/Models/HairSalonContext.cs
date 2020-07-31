aa1dbfe1f8563fb610f19e674106563e8327516c
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-31 11:08:26 -0600

    add remaining Controllers and Models.

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+  public class HairSalonContext : DbContext
+  {
+    public virtual DbSet<Stylist> Stylists { get; set; }
+
+    // public virtual DbSet<Review> Reviews { get; set; }
+
+    public DbSet<Client> Clients { get; set; }
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
+  }
+}
